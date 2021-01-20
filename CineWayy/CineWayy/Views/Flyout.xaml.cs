3270a129e870d1d3d0ab455384274887bdd14fd6
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2021-01-20 04:27:08 -0300

    Atualizando para Flyout o menu lateral.

---
+++ CineWayy/CineWayy/Views/Flyout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CineWayy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Flyout : FlyoutPage
+    {
+        public Flyout()
+        {
+            InitializeComponent();
+        }
+    }
+}
